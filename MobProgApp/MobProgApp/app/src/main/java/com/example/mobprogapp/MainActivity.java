1a89f64ba77d5377e0e894da3a365314cd874cb4
kfvertucio <106854021+Kal1208@users.noreply.github.com>
2023-08-29 11:08:58 +0800

    Add files via upload

---
+++ MobProgApp/MobProgApp/app/src/main/java/com/example/mobprogapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.mobprogapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void ButtonClick(View view) {
+
+        Toast.makeText(this, "Hello Kalvin Vertucio!", Toast.LENGTH_SHORT).show();
+    }
+}
